c259010576da7dbe4e0f92f6ae49c392663129a7
satyam-coder07 <satyamswarnakar0@gmail.com>
2025-12-08 00:33:01 +0530

    Update Profile.jsx

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -18,7 +18,7 @@ const Profile = () => {
 
     const fetchProfile = async () => {
         try {
-            const res = await api.get('/auth/profile');
+            const res = await api.get('api/auth/profile');
             setProfile(res.data);
         } catch (err) {
             console.error(err);
